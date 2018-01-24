995b381b8d9581842ee0036294ebd8bc9c7ba967
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-24 13:16:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(str_input)
-  if str_input=="I LOVE YOU GRANDMA!" {
+  if str_input=="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  } elsif str_input==str_input.upcase {
+  elsif str_input==str_input.upcase
     return "NO, NOT SINCE 1938!"
-  } else {
+  else
     return "HUH?! SPEAK UP, SONNY!"
-  }
+  end
 end
